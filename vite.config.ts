a9bd634610a516807d05bf33ffbf5aa8b04c7140
sovaz1997 <sovaz1997@yandex.ru>
2023-01-14 12:21:26 +0300

    Configure Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, InlineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  }
+} as VitestConfigExport)
