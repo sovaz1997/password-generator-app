1fe87c3e5580cbf7291e79dbdc0f926a0b77d798
sovaz1997 <sovaz1997@yandex.ru>
2023-01-14 13:18:09 +0300

    Fix eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,3 @@
-import React, { useState } from 'react'
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((c) => c + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
-};
+const App = () => <div />;
 
 export default App;
