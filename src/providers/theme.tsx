a73b5a3c3fbc85f77f8c73ad55bca85cedf97a90
sovaz1997 <sovaz1997@yandex.ru>
2023-01-15 16:42:04 +0300

    Change breakpoints

--- src/providers/theme.tsx
+++ src/providers/theme.tsx
@@ -62,11 +62,21 @@ const theme = createTheme({
       },
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 375,
+      md: 768,
+      lg: 1440,
+      xl: 1600,
+    },
+  },
 });
 
 const globalStyles = createStyles(() => ({
   body: {
     backgroundColor: theme.palette.greyVeryDark,
+    userSelect: 'none',
   },
 }));
 
